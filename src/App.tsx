5bc215a70d8ec3be5b449fb23b1bf03ff8de0d25
Ego Maragustaf <egomaragustaf@gmail.com>
2024-05-30 17:07:17 +0700

    chore: add shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "~/components/ui/button";
+import { ModeToggle } from "~/components/ui/mode-toggle";
+
 export default function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center min-h-screen gap-4">
       <h1>Hello World</h1>
+      <Button>Click Me!</Button>
+      <ModeToggle />
     </div>
   );
 }
