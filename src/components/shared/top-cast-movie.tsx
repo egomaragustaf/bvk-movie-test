2d3e535130118c2e2b5805534081b176dfeb77ef
Ego Maragustaf <egomaragustaf@gmail.com>
2024-06-03 22:54:49 +0700

    fix: unused value credit

--- src/components/shared/top-cast-movie.tsx
+++ src/components/shared/top-cast-movie.tsx
@@ -1,13 +1,13 @@
 import { Movie } from "~/types";
 import CastMovie from "~/components/shared/cast-movie";
 
-export function TopCastMovie({ credits }: { credits: Movie["credits"] }) {
+export function TopCast({ credits }: { credits: Movie["credits"] }) {
   return (
     <div className="mb-10">
       <h3 className="text-xl font-bold mb-5">Top Cast</h3>
       <div className="grid grid-cols-2">
         {credits?.cast
-          .filter((credit, key) => key < 18)
+          .filter((_credit, key) => key < 18)
           .map((cast, key) => {
             return (
               <div key={key} className="py-2">
